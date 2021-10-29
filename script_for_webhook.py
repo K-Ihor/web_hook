ac3c9b1334d785f07fbee31e75ccdf3b72534257
K-Ihor <i.kucherov.m@gmail.com>
2021-10-29 15:37:52 +0200

    version0.1

--- script_for_webhook.py
+++ script_for_webhook.py
@@ -7,7 +7,9 @@ app = Flask(__name__)
 @app.route('/', methods=['POST'])
 def foo():
     data = json.loads(request.data)
+    print(f"#############################################/n{data}/n########################")
     print("New commit by: {}".format(data['commits'][0]['author']['name']))
+    print("####  OK  #####")
     return "OK"
 
 
